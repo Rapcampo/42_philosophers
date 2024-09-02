fc370de56199952203ef5b3f55f7e4172752e590
Rapcampo <raphael.campospt@gmail.com>
2024-09-02 01:44:41 +0100

    enough for today

---
+++ srcs/philo.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 22:01:22 by rapcampo          #+#    #+#             */
+/*   Updated: 2024/09/02 00:29:04 by rapcampo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+#include <pthread.h>
+#include <stddef.h>
+#include <unistd.h>
+
+t_philo		*philo_init(t_table *table);
+void		*philo_routine(void *ptr);
+static size_t	philo_eat_routine(t_philo *philo);
+static void	philo_get_fork(t_philo *philo, t_philo *philo_fork);
+
+t_philo	*philo_init(t_table *table)
+{
+	size_t	now;
+
+	auto int i = table->num_philo;
+	auto t_philo *res = malloc(sizeof(t_philo) * i);
+	if (!res)
+		return (table->err++, NULL);
+	memset(res, 0, sizeof(t_philo) * i);
+	now = get_time_curr();
+	while (i--)
+	{
+		res[i].philo_id = i + 1;
+		res[i].last_meal = now;
+		res[i].fork = 1;
+		res[i].time_to_eat = table->time_to_eat;
+		res[i].time_to_sleep = table->time_to_sleep;
+		pthread_mutex_init(&res[i].mutex, NULL);
+		res[i].next = &res[(i + 1) % table->num_philo];
+		res[i].state = table;
+	}
+	return (res);
+}
+
+void	*philo_routine(void *ptr)
+{
+	t_philo *philo;
+
+	philo = (t_philo *)ptr;
+	while (1)
+	{
+		if (philo_eat_routine(philo))
+			break ;
+		pthread_mutex_lock(&philo->mutex);
+		if (philo->dead)
+			break ;
+		philo->meals_eaten++;
+		philo->sleeping++;
+		pthread_mutex_unlock(&philo->mutex);
+		usleep(philo->time_to_sleep * 1000);
+		if (philo->dead)
+			break ;
+		philo->thinking++;
+		pthread_mutex_unlock(&philo->mutex);
+	}
+	pthread_mutex_unlock(&philo->mutex);
+	return (NULL);
+}
+
+static size_t	philo_set_routine(t_philo *philo)
+{
+	philo_get_fork(philo, philo);
+	philo_get_fork(philo, philo->next);
+	pthread_mutex_lock(&philo->mutex);
+	if (philo->dead)
+		return (1);
+	philo->eating++;
+	philo->last_meal = get_time_curr();
+	pthread_mutex_unlock(&philo->mutex);
+	usleep(philo->time_to_eat * 1000);
+	pthread_mutex_lock(&philo->mutex);
+	philo->fork++;
+	pthread_mutex_unlock(&philo->mutex);
+	pthread_mutex_lock(&philo->mutex);
+	philo->next->fork++;
+	pthread_mutex_unlock(&philo->mutex);
+	return (0);
+}
+
+static void	philo_get_fork(t_philo *philo, t_philo *philo_fork)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&philo->mutex);
+		if (philo->dead)
+		{
+			pthread_mutex_unlock(&philo->mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&philo->mutex);
+		pthread_mutex_lock(&philo->mutex);
+		if (!philo_fork->fork)
+		{
+			pthread_mutex_unlock(&philo->mutex);
+			usleep(1000);
+			continue ;
+		}
+		philo_fork->fork--;
+		pthread_mutex_unlock(&philo->mutex);
+		pthread_mutex_lock(&philo->mutex);
+		philo->in_hand++;
+		pthread_mutex_unlock(&philo->mutex);
+		break ;
+	}
+}
--- srcs/utils.c
+++ srcs/utils.c
@@ -10,4 +10,62 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../includes/philo.h"
+
+static size_t	ft_atoi(char *num, size_t *err);
+size_t			get_time_curr(void);
+size_t			get_time(void);
+void			get_input(int argc, char **argv, t_table *table);
+
+static size_t	ft_atoi(char *num, size_t *err)
+{
+	size_t	res;
+
+	if (!(*num >='0' && *num <= '9'))
+		return ((*err)++);
+	res = 0;
+	while (*num && (*num >= '0' && *num <= '9'))
+		res = (res * 10) + (*num++ - '0');
+	if (res > INT_MAX || res == 0)
+		return ((*err)++);
+	return (res);
+}
+
+size_t	get_time_curr(void)
+{
+	struct timeval	t;
+
+	if (gettimeofday(&t, NULL))
+		return (putfd(RED BLN"ERROR! Could not get time"RST, 2), 0);
+	return ((t.tv_sec * 1000 + t.tv_usec / 1000));
+}
+
+size_t	get_time(void)
+{
+	static size_t	start;
+	size_t			now;
+
+	now = get_time_curr();
+	if (!start)
+		start = now;
+	return (now - start);
+}
+
+void	get_input(int argc, char **argv, t_table *table)
+{
+	table->argc = argc;
+	table->num_philo = ft_atoi(argv[0], &table->err);
+	table->time_to_die = ft_atoi(argv[1], &table->err);
+	table->time_to_eat = ft_atoi(argv[2], &table->err);
+	table->time_to_sleep = ft_atoi(argv[3], &table->err);
+	if (argc == 5)
+		table->max_eat = ft_atoi(argv[4], &table->err);
+	get_time();
+}
+
+void	putfd(char *str, int fd)
+{
+	auto int len = -1;
+	while (str[++len]);
+	write(fd, str, len);
+}
